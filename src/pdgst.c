e126d7cb3ca2bee32b041d4e07ec9cfba17aae7f
zmoelnig <zmoelnig@5c11c4bb-bd2d-0410-856d-cfe5d47ff667>
2009-03-03 14:54:04 +0000

    ...
    git-svn-id: https://svn.umlaeute.mur.at/svnroot/zmoelnig/projects/pdgst@490 5c11c4bb-bd2d-0410-856d-cfe5d47ff667

--- src/pdgst.c
+++ src/pdgst.c
@@ -15,11 +15,7 @@
 /* the pdgst base-class */
 typedef struct _pdgst_core
 {
-  GstElement*x_element;
-  unsigned int x_numinlets;
-  t_inlet**x_inlet;
-  unsigned int x_numoutlets;
-  t_outlet**x_outlet;
+  GstElement*element;
 } t_pdgst_core;
 
 
@@ -28,37 +24,111 @@ static t_class*pdgst_class;
 typedef struct _pdgst
 {
   t_object x_obj;
-  t_pdgst_core*x_element;
+  t_pdgst_core*x_gst;
 
-  t_inlet*x_ctlin;
-  t_outlet*x_ctlout;
+  t_outlet*x_infout;
 } t_pdgst;
 
-static void pdgst_free(t_pdgst*x) {
 
+/* should be "static t_atom*" */
+static void pdgst_getparam(t_pdgst*x, t_symbol*s)
+{
+  if(x->x_gst&&x->x_gst->element) {
+    GstElement*element=x->x_gst->element;
+    GValue v;
+    GType t;
+
+    t_symbol*s0;
+    t_float f;
+    t_atom a;
+    int success=0;
+
+    g_object_get_property(G_OBJECT (element), s->s_name, &v);
+
+    if(G_VALUE_HOLDS(&v,G_TYPE_STRING)) {
+         success=1;
+         s0=gensym(g_value_get_string(&v));
+         SETSYMBOL(&a, s0);
+    } else if (G_VALUE_HOLDS(&v,G_TYPE_INT)) {
+      success=1;
+      f=g_value_get_int(&v);
+      SETFLOAT(&a, f);
+    } else if  (G_VALUE_HOLDS(&v,G_TYPE_FLOAT)) {
+      success=1;
+      f=g_value_get_float(&v);
+      SETFLOAT(&a, f);
+    } else {
+      pd_error(x, "don't know what to do...");
+      success=0;
+    }
+    
+    if(success) {
+      outlet_list(x->x_infout, 0, 1, &a);
+    }
+    g_value_unset(&v);
+  } else {
+    // no element
+  }
+}
+
+static void pdgst_setparam(t_pdgst*x, t_symbol*s, t_atom*ap)
+{
+  if(x->x_gst&&x->x_gst->element) {
+    GstElement*element=x->x_gst->element;
+    switch(ap->a_type) {
+    case A_FLOAT:
+      g_object_set (G_OBJECT (element), s->s_name, atom_getfloat(ap), NULL);
+      break;
+    case A_SYMBOL: default:
+      g_object_set (G_OBJECT (element), s->s_name, atom_getsymbol(ap)->s_name, NULL);
+      break;
+    }
+  } else {
+    // no element
+  }
+}
+
+static void pdgst_core_free(t_pdgst_core*gstcore)
+{
+  if(!gstcore)return;
+  if(gstcore->element) {
+    gst_object_unref (GST_OBJECT (gstcore->element));
+  }
+}
+
+static t_pdgst_core*pdgst_core_new(GstElement*lmn, t_object*obj)
+{
+  t_pdgst_core*ret=(t_pdgst_core*)getbytes(sizeof(t_pdgst_core));
+  ret->element=lmn;
+  return ret;
+}
+
+
+static void pdgst_free(t_pdgst*x) {
+  if(x->x_gst)
+    pdgst_core_free(x->x_gst);
+  x->x_gst=NULL;
+  outlet_free(x->x_infout);
 }
 
 static void *pdgst_new(t_symbol*s, int argc, t_atom* argv) {
   t_pdgst*x=(t_pdgst*)pd_new(pdgst_class);
-  x->x_numinlets=0;
-  x->x_numoutlets=0;
-  x->x_inlet=NULL;
-  x->x_outlet=NULL;
-
-  x->x_element=NULL;
-
+  post("pdgst_new: %s", s->s_name);
+  x->x_gst=NULL;
+  x->x_infout=NULL;
 
   if(gensym("pdgst")==s) {
     /* LATER make a controller.... */
+
   } else {
-    x->x_element=gst_element_factory_make(s->s_name, NULL);
-    if(NULL==x->x_element) {
-    
+    GstElement*lmn=gst_element_factory_make(s->s_name, NULL);
+    if(NULL==lmn) {
+      post("factory failed to create element...'%s'", s->s_name);
       return NULL;
     }
-
-    
-
+    x->x_infout=outlet_new(&x->x_obj, 0);
+    x->x_gst=pdgst_core_new(lmn, &x->x_obj);
+    post("created gstelement");
   }
 
 
@@ -137,7 +207,7 @@ void pdgst_setup(void)
                         (t_newmethod)pdgst_new,
                         (t_method)pdgst_free,
                         sizeof(t_pdgst),
-                        CLASS_NOINLET, 
+                        0 /* CLASS_NOINLET */,
                         A_GIMME, 0);
 }
 
