d376065afa86612a5ba1e751ec638def78e2779e
zmoelnig <zmoelnig@5c11c4bb-bd2d-0410-856d-cfe5d47ff667>
2009-07-09 19:59:30 +0000

    base gem files (non-functional)
    git-svn-id: https://svn.umlaeute.mur.at/svnroot/zmoelnig/projects/pdgst@647 5c11c4bb-bd2d-0410-856d-cfe5d47ff667

--- src/gem/pix_gst2pix.h
+++ src/gem/pix_gst2pix.h
@@ -2,31 +2,20 @@
 
     GEM - Graphics Environment for Multimedia
 
-    Interface to the VideoIO framework
-    this object is for video file reading
-    
-    Copyright (c) 2007 Thomas Holzmann, Georg Holzmann
     For information on usage and redistribution, and for a DISCLAIMER OF ALL
     WARRANTIES, see the file, "GEM.LICENSE.TERMS" in this distribution.
 
     -----------------------------------------------------------------*/
 
-#ifndef INCLUDE_PIX_FILEREAD_H_
-#define INCLUDE_PIX_FILEREAD_H_
-#include "Base/config.h"
+#ifndef INCLUDE_PIX_GST2PIX_H_
+#define INCLUDE_PIX_GST2PIX_H_
 
-#define MAX_FILM_HANDLES 8
-
-#include <stdio.h>
-
-#include "Base/GemBase.h"
-#include "Base/GemPixUtil.h"
-#include "VIOKernel.h"
+#include "pdgstGem.h"
 
 /*-----------------------------------------------------------------
     
   CLASS
-    pix_file_read
+    pix_gst2pix
     
     Loads in a movie with the videoIO framework
     
@@ -36,93 +25,25 @@
     DESCRIPTION
 
 -----------------------------------------------------------------*/
-class GEM_EXTERN pix_file_read : public GemBase
+class GEM_EXTERN pix_gst2pix : public pdgstGem
 {
-  CPPEXTERN_HEADER(pix_file_read, GemBase)
+  CPPEXTERN_HEADER(pix_gst2pix, pdgstGem)
     
   public:  
     
     //////////
     // Constructor
-    pix_file_read(t_symbol *filename);
+    pix_gst2pix(void);
 
   protected:
     
     //////////
     // Destructor
-    virtual ~pix_file_read();
+    virtual ~pix_gst2pix();
 
     //////////
-    // close the movie file
-    virtual void closeFile(void);
-    
-    //////////
-    // open a movie up
-    virtual void openFile(t_symbol *filename);
-
-    //////////
-    // DSP-Message
-    virtual void dspMess(void *data, t_signal** sp);
-
-    //////////
-    // DSP perform
-    static t_int* perform(t_int* w);
-
-    //////////
-    // Do the rendering
+    // fetch an image from the gst-pipeline and  output it as pix
     virtual void render(GemState *state);
-
-    //////////
-    // Clear the dirty flag on the pixBlock
-    virtual void postrender(GemState *state);
-
-    //////////
-    // force a specific colorspace
-    virtual void forceColorspace(t_symbol *cs);
-
-  //-----------------------------------
-  // GROUP:	Movie data
-  //-----------------------------------
-
-    //////////
-    // a outlet for information like #frames and "reached end"
-    t_outlet     *m_outNumFrames;
-    t_outlet     *m_outEnd;
-    // the audio outlets~
-    t_outlet *m_outAudio[2];
-
-    // the file reader
-    VideoIO_::FileRead *fileReader;
-    VideoIO_::VIOKernel m_kernel;
-    
-    // here the frame is stored
-    pixBlock m_image;
-    // true if we loaded a new film
-    bool m_newfilm;
-    // true if stopband already sent
-    bool m_already_banged;
- 
-  protected:
-    
-    // reallocate frame data
-    void reallocate_m_image(void);
-
-    void infoSize(void);
-    
-    //////////
-    // static member functions
-    static void openMessCallback(void *data, t_symbol *s);
-    static void startCallback(void *data, t_floatarg start);
-    static void stopCallback(void *data, t_floatarg stop);
-    static void seekCallback(void *data, t_floatarg seek);
-    static void speedCallback(void *data, t_floatarg speed);
-    static void csCallback(void *data, t_symbol *s);
-    static void atrackCallback(void *data, t_floatarg track);
-    static void vtrackCallback(void *data, t_floatarg track);
-    static void audioIOCallback(void *data, t_floatarg io);
-
-    // audio callback
-    static void dspMessCallback(void *data,t_signal **sp);
 };
 
 #endif	// for header file
--- src/gem/pix_pix2gst.cpp
+++ src/gem/pix_pix2gst.cpp
@@ -1,15 +1,5 @@
 //////////////////////////////////////////////////////////////////////////
 //
-//   VideoIO-Framework for GEM/PD
-//
-//   Writes a digital video (like AVI, Mpeg, Quicktime) to the harddisc.
-//
-//   FileWrite
-//   header file
-//
-//   copyright            : (C) 2007 by Thomas Holzmann, Georg Holzmann
-//   email                : holzi1@gmx.at, grh _at_ mut _dot_ at
-//
 //   This program is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation; either version 3 of the License, or
@@ -17,68 +7,20 @@
 //
 ///////////////////////////////////////////////////////////////////////////
 
-#include "pix_file_write.h"
+#include "pix_pix2gst.h"
 
-#include "Base/GemMan.h"
+CPPEXTERN_NEW(pix_pix2gst)
 
-CPPEXTERN_NEW_WITH_ONE_ARG(pix_file_write, t_symbol *, A_DEFSYM)
-
-pix_file_write :: pix_file_write(t_symbol *filename):
-    fileWriter(NULL), 
-    m_recording(false), 
-    m_first_time(false)
+pix_pix2gst :: pix_pix2gst(void):
 {
-  if(gensym("")!=filename) 
-    openFile(filename);
 }
 
-pix_file_write :: ~pix_file_write()
+pix_pix2gst :: ~pix_pix2gst()
 {
-  closeFile();
 }
-void pix_file_write :: closeFile(void)
+void pix_pix2gst :: render(GemState *state)
 {
-  m_recording = false;
-  if(fileWriter) {
-    fileWriter->stopRecording();
-    delete fileWriter;
-  }
-  fileWriter=NULL;
-}
-
-void pix_file_write :: openFile(t_symbol *filename)
-{
-  closeFile();
-
-  // make the right filename
-  char tmp_buff[MAXPDSTRING];
-  char *path=tmp_buff;
-  canvas_makefilename(getCanvas(), filename->s_name, tmp_buff, MAXPDSTRING);
-  if (FILE*fd=fopen(tmp_buff, "r")) fclose(fd);
-  else path=filename->s_name;
-
-  // get the FileWrite plugin
-  // TODO: the fileWriter should be returned based on the <file>
-  // and not the other way round...
-  fileWriter = m_kernel.getFileWriteServer().getPlugin();
-
-  if(fileWriter) {
-    // open file
-    if( !fileWriter->openFile(path) )
-      {
-        error("could not open file %s", path);
-        closeFile();
-        return;
-      }
-  } else {
-    error("couldn't find (suitable) VideoIO plugin for writing '%s'!", path);
-    return;
-  }
-
-}
-
-void pix_file_write :: render(GemState *state)
-{
+#if 0
   if(!fileWriter ) return;
   if(!m_recording) return;
   if(!state || !state->image)return;
@@ -132,52 +74,6 @@ void pix_file_write :: render(GemState *state)
                        m_frame.getYSize(), m_frame.getColorSize());
 
   fileWriter->pushFrame(m_frame);
+#endif
 }
 
-
-/////////////////////////////////////////////////////////
-// static member functions
-//
-/////////////////////////////////////////////////////////
-
-void pix_file_write :: obj_setupCallback(t_class *classPtr)
-{
-  class_addmethod(classPtr, (t_method)&pix_file_write::openMessCallback,
-		  gensym("open"), A_DEFSYM, A_NULL);
-
-  class_addmethod(classPtr, (t_method)&pix_file_write::startCallback,
-                  gensym("start"), A_DEFFLOAT, A_NULL);
-  class_addmethod(classPtr, (t_method)&pix_file_write::stopCallback,
-                  gensym("stop"), A_DEFFLOAT, A_NULL);
-  class_addmethod(classPtr, (t_method)&pix_file_write::setCodecCallback,
-		  gensym("codec"), A_GIMME, A_NULL);
-  class_addmethod(classPtr, (t_method)&pix_file_write::getCodecCallback,
-		  gensym("getCodec"), A_NULL);
-}
-
-void pix_file_write :: openMessCallback(void *data, t_symbol*s)
-{
-  GetMyClass(data)->openFile(s);
-}
-
-void pix_file_write :: startCallback(void *data, t_floatarg start)
-{
-  GetMyClass(data)->m_recording = true;
-  GetMyClass(data)->m_first_time = true;
-}
-
-void pix_file_write :: stopCallback(void *data, t_floatarg stop)
-{
-  GetMyClass(data)->m_recording = false;
-  GetMyClass(data)->closeFile();
-}
-
-void pix_file_write :: setCodecCallback(void *data, t_symbol *s, int argc, t_atom *argv)
-{
-  GetMyClass(data)->fileWriter->setCodec(argc,argv); 
-}
-
-void pix_file_write :: getCodecCallback(void *data)
-{
-  GetMyClass(data)->fileWriter->getCodec(); 
-}
